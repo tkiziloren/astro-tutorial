e7a11c7cf491d0296fbfb8be8b7bcf7058fdc8a0
Tevfik Kiziloren <tkiziloren@gmail.com>
2024-09-16 11:25:47 +0100

    Finishing the tutorial.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-
+  site: "https://tiny-concha-85139d.netlify.app/",
+  integrations: [preact()]
 });
